bc2d7e948227fb8118e80fb1be865b730e8dff1b
nathangiose <nathangiose@gmail.com>
2021-10-19 10:10:39 +0200

    template refs

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,8 @@
   <!-- <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   <h1>{{ title }}</h1>
+  <input type="text" ref="name">
+  <button @click="handleClick">Click me</button>
 </template>
 
 <script>
@@ -13,6 +15,13 @@ export default {
     return {
       title: 'This is my Vue app :)'
     } 
+  },
+  methods: {
+    handleClick() {
+      console.log(this.$refs.name)
+      this.$refs.name.classList.add('active')
+      this.$refs.name.focus()
+    }
   }
   // components: {
   //   HelloWorld
